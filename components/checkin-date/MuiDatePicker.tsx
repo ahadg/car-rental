f1e3a5cee081471829829577d390d9dbd3392b1a
Muhammad Ahad <muhmmadahad594@gmail.com>
2023-01-19 22:27:06 +0500

    mobile repsonsive calender added

--- components/checkin-date/MuiDatePicker.tsx
+++ components/checkin-date/MuiDatePicker.tsx
@@ -4,13 +4,14 @@ import TextField from '@mui/material/TextField';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
 
 export default function BasicDateRangePicker({placeholder,setvalue})  {
     const [value, setValue] : any = React.useState([null, null]);
 
     return (
       <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DatePicker
+      <MobileDatePicker
         label={placeholder}
         value={value}
         onChange={(newValue) => {
